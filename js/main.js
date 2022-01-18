5864437cb02aae5386906c5542452a9791882c22
Trevor - FanshaweC <tvanrys@fanshaweonline.ca>
2022-01-18 12:00:46 -0500

    adding SVG

--- js/main.js
+++ js/main.js
@@ -4,15 +4,19 @@
 
 (() => {
   // make associations (select) with elements in the HTML markup using CSS selectors
-  let theHeading = document.querySelector(".main-heading");
+  let theHeading = document.querySelector(".main-heading"),
+      theIcons = document.querySelectorAll(".icon");
 
   // script your behaviour with functions
   function logElement() {
-    console.log('clicked on the heading');
+    console.log('clicked on an element: ', this.id);
   }
 
   // add event handling here (user "triggers")
   // assign the event you want to listen for, and the function that should be run
   theHeading.addEventListener("click", logElement);
 
+  // loop through all of the icons and add event handling to them
+  // process each icon one at a time and add behaviour
+  theIcons.forEach(icon => icon.addEventListener("click", logElement));
 })();
