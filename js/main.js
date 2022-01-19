60f3135f4c4c3881d23f24c49ee7253b8dcaf634
Trevor - FanshaweC <tvanrys@fanshaweonline.ca>
2022-01-19 14:55:50 -0500

    adding grid and more JS

--- js/main.js
+++ js/main.js
@@ -4,15 +4,22 @@
 
 (() => {
   // make associations (select) with elements in the HTML markup using CSS selectors
-  let theHeading = document.querySelector(".main-heading");
+  let theHeading = document.querySelector(".main-heading"),
+      theIcons = document.querySelectorAll(".icon");
+
+  // debugger; // this is like a breakpoint / pause point in your code
 
   // script your behaviour with functions
   function logElement() {
-    console.log('clicked on the heading');
+    // the "this" keyword refers to the thing that invokes the function
+    // so the "thing" that you click on, mouse over, interact with on the page
+    console.log('clicked on an element:', this.id);
   }
 
-  // add event handling here (user "triggers")
+  // add event handling here (user "triggers") - this is a 1 to 1 relationship
   // assign the event you want to listen for, and the function that should be run
   theHeading.addEventListener("click", logElement);
 
+  // one to many relationship (selecting a group of elements) - you have to process this collection and work with each element sequentially in a loop
+  theIcons.forEach(icon => icon.addEventListener("click", logElement));
 })();
